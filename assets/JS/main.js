fa3791626c0229761c2f02597324eb7f6a5edd91
LAPTOP-92NBAG1S\lenovo <nomai6789@gmail.com>
2023-04-08 20:09:55 +0700

    Finish content, add modal Sign Up, add modal Sign In, add 2 file html moi

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -29,7 +29,17 @@ const dryGroups = $$(".dry-fruits .group");
 
 console.log(importGroups, premiumGroups, specialtyGroups, dryGroups);
 
-const groupWidth = importGroups[0].offsetWidth;
+//const groupWidth = importGroups[0].offsetWidth;
+//Chuyển động phù hợp với kích cỡ màn hình
+var groupWidth;
+var screenWidth = document.documentElement.clientWidth;
+if(screenWidth < 740)
+    groupWidth = 340;
+else if(screenWidth >= 740 && screenWidth < 1024)
+    groupWidth = 740;
+else if(screenWidth >= 1024)
+    groupWidth = 1170;
+    
 const groupLength = importGroups.length;
 
 let index = 0;
@@ -91,3 +101,61 @@ function updateGroups(index, groups) {
         group.style.transform = `translateX(${distance}px)`;
     });
 }
+
+
+
+//Modal Sign Up
+const signUpBtns = document.querySelectorAll('.SignUp');
+const modal_SignUp = document.querySelector('.modal-SignUp');
+const modal_SignUpContainer = document.querySelector('.modal-SignUp-container');
+const modal_SignUpClose = document.querySelector('.modal-SignUp-close');
+
+
+//Modal Sign In
+const signInBtns = document.querySelectorAll('.SignIn');
+const modal_SignIn = document.querySelector('.modal-SignIn');
+const modal_SignInContainer = document.querySelector('.modal-SigIn-container');
+const modal_SignInClose = document.querySelector('.modal-SignIn-close');
+
+//Hàm hiển thị modal
+function showSignUp(){
+    modal_SignUp.classList.add('open');
+}
+function showSignIn(){
+    modal_SignIn.classList.add('open');
+}
+
+//hàm ẩn modal
+function hideSignUp(){
+    modal_SignUp.classList.remove('open');
+}
+function hideSignIn(){
+    modal_SignIn.classList.remove('open');
+}
+
+//lặp qua từng thẻ button và nghe hành vi click
+for(const signUpBtn of signUpBtns){
+    signUpBtn.addEventListener('click',showSignUp);
+}
+for(const signInBtn of signInBtns){
+    signInBtn.addEventListener('click',showSignIn);
+}
+
+//Nghe hành vi khi click chuột vào button close
+modal_SignUpClose.addEventListener('click',hideSignUp);
+modal_SignInClose.addEventListener('click',hideSignIn);
+
+//Bấm bất kỳ, cửa sổ modal sẽ đog lại
+modal_SignUp.addEventListener('click',hideSignUp);
+// modal_SignIn.addEventListener('click',hideSignIn);
+
+// //Ngừng việc nổi bọt, làm cho khi click chuột vào containner nó không bị mất
+modal_SignUpContainer.addEventListener('click',function(event){
+    event.stopPropagation();
+})
+modal_SignInContainer.addEventListener('click',function(event){
+    event.stopPropagation();
+})
+
+
+
